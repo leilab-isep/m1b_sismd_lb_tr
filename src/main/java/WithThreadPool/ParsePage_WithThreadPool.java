6667ab44c50eb3a781d0be26aa8ba25855f00c54
leilaboaze <1240470@isep.ipp.pt>
2025-05-10 14:35:40 +0100

    #2: implement word count with threadpool

---
+++ src/main/java/WithThreadPool/ParsePage_WithThreadPool.java
@@ -0,0 +1,29 @@
+package WithThreadPool;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+public class ParsePage_WithThreadPool implements Callable<Map<String,Integer>> {
+    private final List<Page_WithThreadPool> pageList;
+
+    public ParsePage_WithThreadPool(List<Page_WithThreadPool> pageList) {
+        this.pageList = pageList;
+    }
+
+
+    @Override
+    public Map<String, Integer> call() throws Exception {
+        Map<String,Integer> localCounts = new HashMap<>();
+        for (Page_WithThreadPool page : pageList) {
+            Iterable<String> words = new Words_WithThreadPool(page.getText());
+            for (String word : words) {
+                if (word.length() > 1 || word.equals("a") || word.equals("I")) {
+                    localCounts.merge(word, 1, Integer::sum);
+                }
+            }
+        }
+        return localCounts;
+    }
+}
--- src/main/java/WithThreadPool/WordCount_WithThreadPool.java
+++ src/main/java/WithThreadPool/WordCount_WithThreadPool.java
@@ -1,9 +1,10 @@
 package WithThreadPool;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class WordCount_WithThreadPool {
@@ -16,31 +17,47 @@ public class WordCount_WithThreadPool {
     public static void main(String[] args) throws Exception {
 
         long start = System.currentTimeMillis();
+        int numberOfThreads = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(numberOfThreads);
         Iterable<Page_WithThreadPool> pages = new Pages_WithThreadPool(maxPages, fileName);
+        int chunkValue = 500;
+        List<Future<Map<String,Integer>>> futures = new ArrayList<>();
+        List<Page_WithThreadPool> pageChunck = new ArrayList<>(chunkValue);
+
         int processedPages = 0;
         for (Page_WithThreadPool page : pages) {
             if (page == null)
                 break;
-            Iterable<String> words = new Words_WithThreadPool(page.getText());
-            for (String word : words)
-                if (word.length() > 1 || word.equals("a") || word.equals("I"))
-                    countWord(word);
-            ++processedPages;
+            pageChunck.add(page);
+            processedPages++;
+            if (pageChunck.size() >= chunkValue) {
+                ParsePage_WithThreadPool parsePage = new ParsePage_WithThreadPool(new ArrayList<>(pageChunck));
+                Future<Map<String, Integer>> future = executor.submit(parsePage);
+                futures.add(future);
+                pageChunck.clear();
+            }
         }
+        if (!pageChunck.isEmpty()) {
+            ParsePage_WithThreadPool parsePage = new ParsePage_WithThreadPool(new ArrayList<>(pageChunck));
+            Future<Map<String, Integer>> future = executor.submit(parsePage);
+            futures.add(future);
+        }
+
+        executor.shutdown();
+
+        for (Future<Map<String,Integer>> future : futures) {
+            Map<String,Integer> partial = future.get();
+            partial.forEach((word, count) ->
+                    counts.merge(word, count, Integer::sum)
+            );
+        }
+
         long end = System.currentTimeMillis();
         System.out.println("Processed pages: " + processedPages);
         System.out.println("Elapsed time: " + (end - start) + "ms");
 
         LinkedHashMap<String, Integer> commonWords = new LinkedHashMap<>();
         counts.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).forEachOrdered(x -> commonWords.put(x.getKey(), x.getValue()));
-        commonWords.entrySet().stream().limit(3).collect(Collectors.toList()).forEach(x -> System.out.println("Word: \'" + x.getKey() + "\' with total " + x.getValue() + " occurrences!"));
-    }
-
-    private static void countWord(String word) {
-        Integer currentCount = counts.get(word);
-        if (currentCount == null)
-            counts.put(word, 1);
-        else
-            counts.put(word, currentCount + 1);
+        commonWords.entrySet().stream().limit(4).collect(Collectors.toList()).forEach(x -> System.out.println("Word: \'" + x.getKey() + "\' with total " + x.getValue() + " occurrences!"));
     }
 }
